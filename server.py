6b95d6cf5e3890d76ccfd8bc5d64b8015fa12b14
Rafael Gozlan <rafael.gozlan@epita.fr>
2016-07-18 14:47:13 +0200

    server: add /actoboard route for upload

--- server.py
+++ server.py
@@ -3,16 +3,29 @@ from flask import Flask
 from flask import Markup
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
- 
+
 # Read port selected by the cloud for our application
 port = int(os.getenv('PORT', 8000))
+
+form = "lol"
 
 @app.route("/")
 def chart():
     labels = ["January","February","March","April","May","June","July","August"]
     values = [10,9,8,7,6,4,7,8]
+    print(render_template('chart.html', values=values, labels=labels))
     return render_template('chart.html', values=values, labels=labels)
- 
+
+@app.route("/actoboard", methods=["GET", "POST"])
+def actoboard():
+    global form
+    if request.method == "POST":
+        form = request.form
+    elif request.method == "GET":
+        return form
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=port)
